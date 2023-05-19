5c793c09a8cece35f7e7dfaae2dab5da11c9f4e4
Cyanne-Jones <jonescyanne@yahoo.com>
2023-05-18 19:20:06 -0600

    Create zustand store

---
+++ src/components/hooks/useHumanStore.js
@@ -0,0 +1,16 @@
+import create from "zustand";
+
+export const useHumanStore = create((set) => ({
+  humans: [],
+  pickedHuman: '',
+  setHumans: (data) => set(() => { 
+    return ({ humans: data });
+  }),
+  setPickedHuman: () => set((state) => {
+
+    const pickedNumber = Math.floor(Math.random() * state.humans.length())
+    const pickedHuman = state.humans[pickedNumber]
+
+    return ({pickedHuman: pickedHuman})
+  })
+}));
