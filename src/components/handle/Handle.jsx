469ff7dec5a3874e5a938ef916d295f977699560
Cyanne-Jones <jonescyanne@yahoo.com>
2023-05-18 19:04:24 -0600

    Style and animate handle

--- src/components/handle/Handle.jsx
+++ src/components/handle/Handle.jsx
@@ -1,9 +1,23 @@
 import './Handle.css'
+import { useState } from 'react'
 
 export const Handle = () => {
+  const [isAnimating, setIsAnimating] = useState(false)
+
+  const handleClick = () => {
+    setIsAnimating(true)
+    setTimeout(()=> {
+      setIsAnimating(false)
+    }, 3000)
+  }
   return (
-    <div className='handle'>
-      Handle
+    <div className='handle' onClick={handleClick}>
+      <div className='shaft'></div>
+      <div className='arm'>
+        <div className={isAnimating ? 'cam-animate cam' : 'cam'}></div>
+        <div className={isAnimating ? 'ball-animate ball' : 'ball'}></div>
+      </div>
+
     </div>
   )
 }
